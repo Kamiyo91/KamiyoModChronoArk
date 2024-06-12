99a0b32433198e3d564c9d3bc54934db9f412489
Kamiyo <kamiyo.k91@gmail.com>
2024-06-13 00:42:39 +0200

    Code Cleaning and Passive Life Steal Hotfix

--- Buff/B_KamiyoMask21341.cs
+++ Buff/B_KamiyoMask21341.cs
@@ -11,19 +11,19 @@ namespace KamiyoMod
     {
         public void AttackEffect(BattleChar hit, SkillParticle SP, int DMG, bool Cri)
         {
-            if (hit is BattleEnemy battleEnemy)
+            if (DMG > 0)
             {
-                if (battleEnemy.SkillQueue.Count == 0 ||
-                    (battleEnemy.SkillQueue[0].CastSpeed != 0 && battleEnemy.SkillQueue[0].CastSpeed != 1)) return;
-                var skill = SelectOne(BattleSystem.instance.AllyTeam.Skills_Deck.Where(x => x.Master == BChar))
-                    .FirstOrDefault();
-                if (skill != null) BattleSystem.instance.AllyTeam.Draw(skill);
-                else BattleSystem.instance.AllyTeam.Draw(1);
+                var healAmount = Misc.PerToNum(DMG, 10f);
+                if (healAmount >= 1) BChar.Heal(BChar, healAmount, false);
             }
 
-            var healAmount = Misc.PerToNum(DMG, 10f);
-            if (healAmount < 1) BChar.Heal(BChar, 1, false);
-            else BChar.Heal(BChar, healAmount, false);
+            if (!(hit is BattleEnemy battleEnemy)) return;
+            if (battleEnemy.SkillQueue.Count == 0 ||
+                (battleEnemy.SkillQueue[0].CastSpeed != 0 && battleEnemy.SkillQueue[0].CastSpeed != 1)) return;
+            var skill = SelectOne(BattleSystem.instance.AllyTeam.Skills_Deck.Where(x => x.Master == BChar))
+                .FirstOrDefault();
+            if (skill != null) BattleSystem.instance.AllyTeam.Draw(skill);
+            else BattleSystem.instance.AllyTeam.Draw(1);
         }
 
         public T SelectOne<T>(params T[] list)
--- KamiyoMod_ModItemKeys.cs
+++ KamiyoMod_ModItemKeys.cs
@@ -1,4 +1,5 @@
 using ChronoArkMod;
+
 namespace KamiyoMod
 {
     public static class ModItemKeys
@@ -6,11 +7,13 @@ namespace KamiyoMod
         public static string Buff_B_KamiyoCounterDraw21431 = "B_KamiyoCounterDraw21431";
         public static string Buff_B_KamiyoCounterMana21431 = "B_KamiyoCounterMana21431";
         public static string Buff_B_KamiyoCounterMana_Ex21431 = "B_KamiyoCounterMana_Ex21431";
-		/// <summary>
-		/// Mask of Perception
-		/// Gain 10% Lifesteal on attacks. Attacking enemies with 1 Action point will make you draw 1 Skill.
-		/// </summary>
+
+        /// <summary>
+        ///     Mask of Perception
+        ///     Gain 10% Lifesteal on attacks. Attacking enemies with 1 Action point will make you draw 1 Skill.
+        /// </summary>
         public static string Buff_B_KamiyoMask21341 = "B_KamiyoMask21341";
+
         public static string SimpleCampDialogue_CampDial_Kamiyo21341_SilverStein = "CampDial_Kamiyo21341_SilverStein";
         public static string SimpleCampDialogue_CampDial_Kamiyo21341_Trisha = "CampDial_Kamiyo21341_Trisha";
         public static string SimpleCampDialogue_CampDial_Kamiyo21341_Ilya = "CampDial_Kamiyo21341_Ilya";
@@ -30,73 +33,84 @@ namespace KamiyoMod
         public static string SimpleCampDialogue_CampDial_Kamiyo21341_Iron = "CampDial_Kamiyo21341_Iron";
         public static string SimpleCampDialogue_CampDial_Kamiyo21341_Miss = "CampDial_Kamiyo21341_Miss";
         public static string SimpleCampDialogue_CampDial_Kamiyo21341_Charon = "CampDial_Kamiyo21341_Charon";
-		/// <summary>
-		/// Kamiyo
-		/// Passive:
-		/// High base evade rate (Active from Lv 1).
-		/// On Dodge overheal 2 HP.
-		/// On Death's Door lower all user's skills cost by 1 and add [Swiftness] effect on them.
-		/// At the start of the battle gain [Mask of Perception] buff.
-		/// </summary>
+
+        /// <summary>
+        ///     Kamiyo
+        ///     Passive:
+        ///     High base evade rate (Active from Lv 1).
+        ///     On Dodge overheal 2 HP.
+        ///     On Death's Door lower all user's skills cost by 1 and add [Swiftness] effect on them.
+        ///     At the start of the battle gain [Mask of Perception] buff.
+        /// </summary>
         public static string Character_Kamiyo21341 = "Kamiyo21341";
+
         public static string SkillEffect_SE_T_S_Kamiyo21341_0 = "SE_T_S_Kamiyo21341_0";
         public static string SkillEffect_SE_T_S_Kamiyo21341_1 = "SE_T_S_Kamiyo21341_1";
         public static string SkillEffect_SE_T_S_Kamiyo21341_2 = "SE_T_S_Kamiyo21341_2";
         public static string SkillEffect_SE_T_S_Kamiyo21341_3 = "SE_T_S_Kamiyo21341_3";
         public static string SkillEffect_SE_T_S_Kamiyo21341_4 = "SE_T_S_Kamiyo21341_4";
         public static string SkillEffect_SE_T_S_Kamiyo21341_6 = "SE_T_S_Kamiyo21341_6";
-		/// <summary>
-		/// Add Countdown +2. If attacked while this skill is in Countdown, dodge the attack,cast the skill on the attacker and restore 1 Mana.
-		/// </summary>
-        public static string SkillExtended_SkillEn_Kamiyo21341_0 = "SkillEn_Kamiyo21341_0";
-		/// <summary>
-		/// Imminent Fight
-		/// </summary>
-        public static string Skill_S_Kamiyo21341_0 = "S_Kamiyo21341_0";
-		/// <summary>
-		/// Lower Slash
-		/// If there is any skill from the user in the Cast List, the cost of this skill is reduced by 1
-		/// </summary>
-        public static string Skill_S_Kamiyo21341_1 = "S_Kamiyo21341_1";
-		/// <summary>
-		/// Charged Horizontal Slash
-		/// Recover 1 Mana for each enemy killed. If this skill attack a single enemy, it deal 10 additional damage
-		/// </summary>
-        public static string Skill_S_Kamiyo21341_2 = "S_Kamiyo21341_2";
-		/// <summary>
-		/// Upper Slash
-		/// If there is any skill from the user in the Cast List, the cost of this skill is reduced by 1
-		/// </summary>
-        public static string Skill_S_Kamiyo21341_3 = "S_Kamiyo21341_3";
-		/// <summary>
-		/// Gathering Self
-		/// If attacked while this skill is in Countdown, dodge the attack,cast the skill on the attacker and draw 1 skill.
-		/// </summary>
-        public static string Skill_S_Kamiyo21341_4 = "S_Kamiyo21341_4";
-		/// <summary>
-		/// Mask of Perception
-		/// Increase Action Count of all enemies by 1, restore 3 Mana and draw 2 skills.
-		/// </summary>
-        public static string Skill_S_Kamiyo21341_5 = "S_Kamiyo21341_5";
-		/// <summary>
-		/// Focus
-		/// If attacked while this skill is in Countdown, dodge the attack,cast the skill on the attacker and restore 1 Mana. If used as Fixed Skill, gain [Swiftness] and reduce it's cost by 1.
-		/// </summary>
-        public static string Skill_S_Kamiyo21341_6 = "S_Kamiyo21341_6";
 
+        /// <summary>
+        ///     Add Countdown +2. If attacked while this skill is in Countdown, dodge the attack,cast the skill on the attacker and
+        ///     restore 1 Mana.
+        /// </summary>
+        public static string SkillExtended_SkillEn_Kamiyo21341_0 = "SkillEn_Kamiyo21341_0";
+
+        /// <summary>
+        ///     Imminent Fight
+        /// </summary>
+        public static string Skill_S_Kamiyo21341_0 = "S_Kamiyo21341_0";
+
+        /// <summary>
+        ///     Lower Slash
+        ///     If there is any skill from the user in the Cast List, the cost of this skill is reduced by 1
+        /// </summary>
+        public static string Skill_S_Kamiyo21341_1 = "S_Kamiyo21341_1";
+
+        /// <summary>
+        ///     Charged Horizontal Slash
+        ///     Recover 1 Mana for each enemy killed. If this skill attack a single enemy, it deal 10 additional damage
+        /// </summary>
+        public static string Skill_S_Kamiyo21341_2 = "S_Kamiyo21341_2";
+
+        /// <summary>
+        ///     Upper Slash
+        ///     If there is any skill from the user in the Cast List, the cost of this skill is reduced by 1
+        /// </summary>
+        public static string Skill_S_Kamiyo21341_3 = "S_Kamiyo21341_3";
+
+        /// <summary>
+        ///     Gathering Self
+        ///     If attacked while this skill is in Countdown, dodge the attack,cast the skill on the attacker and draw 1 skill.
+        /// </summary>
+        public static string Skill_S_Kamiyo21341_4 = "S_Kamiyo21341_4";
+
+        /// <summary>
+        ///     Mask of Perception
+        ///     Increase Action Count of all enemies by 1, restore 3 Mana and draw 2 skills.
+        /// </summary>
+        public static string Skill_S_Kamiyo21341_5 = "S_Kamiyo21341_5";
+
+        /// <summary>
+        ///     Focus
+        ///     If attacked while this skill is in Countdown, dodge the attack,cast the skill on the attacker and restore 1 Mana.
+        ///     If used as Fixed Skill, gain [Swiftness] and reduce it's cost by 1.
+        /// </summary>
+        public static string Skill_S_Kamiyo21341_6 = "S_Kamiyo21341_6";
     }
 
     public static class ModLocalization
     {
-		/// <summary>
-		/// Korean:
-		/// English:
-		/// I see, this is the truth. I know that you can do it, good luck.
-		/// Japanese:
-		/// Chinese:
-		/// Chinese-TW:
-		/// </summary>
-        public static string ProgramMasterFinalKamiyo => ModManager.getModInfo("KamiyoMod").localizationInfo.SystemLocalizationUpdate("ProgramMasterFinalKamiyo");
-
+        /// <summary>
+        ///     Korean:
+        ///     English:
+        ///     I see, this is the truth. I know that you can do it, good luck.
+        ///     Japanese:
+        ///     Chinese:
+        ///     Chinese-TW:
+        /// </summary>
+        public static string ProgramMasterFinalKamiyo => ModManager.getModInfo("KamiyoMod").localizationInfo
+            .SystemLocalizationUpdate("ProgramMasterFinalKamiyo");
     }
 }
